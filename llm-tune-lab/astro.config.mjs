61742cd4942fd5f927cf59c8260c86699f0284d1
George Michailov <george.michailov.work@outlook.com>
2026-05-20 21:24:32 -0700

    Changed deployment status.

--- llm-tune-lab/astro.config.mjs
+++ llm-tune-lab/astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-	site: 'https://llmtunelab.com',
+	site: 'https://gmichailov.github.io',
+	base: '/llmtunelab-website',
 	integrations: [mdx()],
 	markdown: {
 		shikiConfig: {
